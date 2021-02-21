288eff089369552cd0bf55ff005199dec3c857c7
Ilyes <ilyes.rayan.benachi@gmail.com>
2021-02-21 20:45:20 +0000

    Design of the game/Correction Bugs/Better Reset Game

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,6 @@
 ﻿var box;
-var topBorder = 24.5;
-var botBorder = -20.5;
+var topBorder = 38;
+var botBorder = -34;
 var direction = new THREE.Vector3();
 clock = new THREE.Clock();
 clock.start();
@@ -14,6 +14,7 @@ function init() {
     //Variable
     //Speed of the ball
 
+    ballReset = true;
     ballSpeed = 1.6;
 
     currentTime = 0;
@@ -21,56 +22,129 @@ function init() {
     playerScore = 0;
     enemyScore = 0;
 
-
     //Create the elements (Use "var" to make it private, and use nothing to make it accessible everywhere)
     player = getBox(1, 4.5, 0.3);
     enemy = getBox(1, 4.5, 0.3);
-    ball = getSphere(0.4);
-    var plane = getPlane(20);
+    ball = getSphere(0.6);
     var directionalLight = getDirectionalLight(1);
-    var helper = new THREE.CameraHelper(directionalLight.shadow.camera);
+    var directionalLight2 = getDirectionalLight(1);
+
+    //Planes to make the line in the middle of the screen
+    var plane = getPlane(0.35, 2);
+    var plane2 = getPlane(0.35, 2);
+    var plane3 = getPlane(0.35, 2);
+    var plane4 = getPlane(0.35, 2);
+    var plane5 = getPlane(0.35, 2);
+    var plane6 = getPlane(0.35, 2);
+    var plane7 = getPlane(0.35, 2);
+    var plane8 = getPlane(0.35, 2);
+    var plane9 = getPlane(0.35, 2);
+    var plane10 = getPlane(0.35, 2);
+    var plane11 = getPlane(0.35, 2);
+    var plane12 = getPlane(0.35, 2);
+    var plane13 = getPlane(0.35, 2);
+    var plane14 = getPlane(0.35, 2);
+    var plane15 = getPlane(0.35, 2);
+    var plane16 = getPlane(0.35, 2);
+    var plane17 = getPlane(0.35, 2);
+    var plane18 = getPlane(0.35, 2);
+    var plane19 = getPlane(0.35, 2);
+    var plane20 = getPlane(0.35, 2);
+    var plane21 = getPlane(0.35, 2);
 
     //Give name to my name to access it outside init()
     plane.name = 'plane-1';
 
     //Position of the elements
-    player.position.x = 33;
+    player.position.x = 40;
     player.position.y = 2;
-    enemy.position.x = -33;
+    player.position.z = 0;
+    enemy.position.x = -40;
     enemy.position.y = 2;
+    enemy.position.z = 0;
 
     ball.position.x = 0;
     ball.position.y = 2;
-    plane.position.z = -10;
+    ball.position.z = 0;
+
+    //Planes to make the line in the middle of the screen
+    plane.position.y = 0;
+    plane2.position.y = 4;
+    plane3.position.y = 8;
+    plane4.position.y = 12;
+    plane5.position.y = 16;
+    plane6.position.y = 20;
+    plane7.position.y = 24;
+    plane8.position.y = 28;
+    plane9.position.y = 32;
+    plane10.position.y = 36;
+    plane21.position.y = 40;
+
+    plane11.position.y = -4;
+    plane12.position.y = -8;
+    plane13.position.y = -12;
+    plane14.position.y = -16;
+    plane15.position.y = -20;
+    plane16.position.y = -24;
+    plane17.position.y = -28;
+    plane18.position.y = -32;
+    plane19.position.y = -36;
+    plane20.position.y = -40;
 
     directionalLight.position.y = 30;
     directionalLight.position.z = 35;
+    directionalLight.position.x = 35;
     directionalLight.intensity = 1.3;
+
+    directionalLight2.position.y = 30;
+    directionalLight2.position.z = 35;
+    directionalLight2.position.x = -35;
+    directionalLight2.intensity = 1.3;
 
     //Add elements to the scene
     scene.add(player);
     scene.add(enemy);
     scene.add(ball);
-    scene.add(plane);
     scene.add(directionalLight);
-    scene.add(helper);
+    scene.add(directionalLight2);
+    //Planes to make the line in the middle of the screen
+    scene.add(plane);
+    scene.add(plane2);
+    scene.add(plane3);
+    scene.add(plane4);
+    scene.add(plane5);
+    scene.add(plane6);
+    scene.add(plane7);
+    scene.add(plane8);
+    scene.add(plane9);
+    scene.add(plane10);
+    scene.add(plane11);
+    scene.add(plane12);
+    scene.add(plane13);
+    scene.add(plane14);
+    scene.add(plane15);
+    scene.add(plane16);
+    scene.add(plane17);
+    scene.add(plane18);
+    scene.add(plane19);
+    scene.add(plane20);
+    scene.add(plane21);
 
     //Settings of the camera
-    camera = new THREE.PerspectiveCamera(140, window.innerWidth / window.innerHeight, 1, 1000);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.y = 2;
-    camera.position.z = 10;
+    camera.position.z = 90;
 
     var renderer = new THREE.WebGLRenderer();
     renderer.shadowMap.enabled = true;
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor('rgb(120, 120, 120)');
+    renderer.setClearColor('rgb(0, 0, 0)');
     document.getElementById("webgl").appendChild(renderer.domElement);
 
     update(renderer, scene, camera);
 
     return scene;
 }
-
 
 //Movement (By Jaber)
 var keyState = {};
@@ -148,7 +222,7 @@ function getBox(w, h, d) {
 
 //To create a sphere
 function getSphere(size) {
-    var geometry = new THREE.SphereGeometry(size, 24, 24);
+    var geometry = new THREE.SphereGeometry(size, 12, 12);
     var material = new THREE.MeshBasicMaterial({
         color: 'rgb(250, 250, 250)'
     });
@@ -161,10 +235,10 @@ function getSphere(size) {
 }
 
 //To create a plane
-function getPlane(size) {
-    var geometry = new THREE.PlaneGeometry(/*width*/size,/*depth*/ size);
+function getPlane(width, depth) {
+    var geometry = new THREE.PlaneGeometry(/*width*/width,/*depth*/ depth);
     var material = new THREE.MeshPhongMaterial({
-        color: 'rgb(120, 120, 120)',
+        color: 'rgb(250, 250, 250)',
         side: THREE.DoubleSide
     });
     var mesh = new THREE.Mesh(
@@ -203,7 +277,6 @@ function getDirectionalLight(intensity) {
     return light;
 }
 
-
 //Update function that runs every frame
 function update(myRenderer, myScene, myCamera) {
     myRenderer.render(
@@ -212,28 +285,41 @@ function update(myRenderer, myScene, myCamera) {
     );
 
     //Respawn the ball
-    if (ball.position.x < -85) {
+    if (ball.position.x < -75 && ballReset) {
         enemyScore++;
-        console.log("goal for enemy: " + enemyScore);
         document.getElementById("enemyScore").innerHTML = enemyScore;
-        ball.position.x = 0;
+        ballReset = false;
+        setTimeout(function () {
+            ball.position.x = 0;
+            ball.position.y = 0;
+            RandomBallDirection();
+            ballSpeed = ballSpeed * (-1);
+            ballReset = true;
+        }, 1500);
     }
-    else if (ball.position.x > 85) {
+    else if (ball.position.x > 75 && ballReset) {
         playerScore++;
-        console.log("goal for player: " + playerScore);
         document.getElementById("playerScore").innerHTML = playerScore;
-        ball.position.x = 0;
+        ballReset = false;
+        setTimeout(function ()
+        {
+            ball.position.x = 0;
+            ball.position.y = 0;
+            RandomBallDirection();
+            ballSpeed = ballSpeed * (-1);
+            ballReset = true;
+        }, 1500);
     }
 
-    if (enemyScore >= 2) {
-        console.log("enemy won!");
-        document.getElementById("messageGame").innerHTML = "You lose!";
+    if (enemyScore >= 5) {
+        document.getElementById("messageGame").innerHTML = "You Lose!";
         ballSpeed = 0;
+        ball.position.y = 100;
     }
-    else if (playerScore >= 2) {
-        console.log("player won!");
-        document.getElementById("messageGame").innerHTML = "You win!";
+    else if (playerScore >= 5) {
+        document.getElementById("messageGame").innerHTML = "You Win!";
         ballSpeed = 0;
+        ball.position.y = 100;
     }
     //Delta (used for collision)
     currentTime = clock.getElapsedTime();
@@ -258,36 +344,45 @@ function update(myRenderer, myScene, myCamera) {
         var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
         var collisionResults = ray.intersectObjects(collidableMeshList);
 
-
         deltaTimeCol = currentTime - oldTime;
 
         //For Debug Purposes:
         //console.log("Time: " + clock.getElapsedTime());
         //console.log("Delta Time: " + deltaTimeCol);
 
-        if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() && deltaTimeCol > 0.5 ) {
+        if (collisionResults.length > 0 && collisionResults[0].distance <= directionVector.length() && deltaTimeCol > 0.5) {
             // Collision detected:
             oldTime = currentTime;
 
             ballSpeed = ballSpeed * (-1);
-            RandomBallDirection()
+            RandomBallDirection();
             //console.log(ballSpeed);
             //console.log("Hit Collision = ");
         }
     }
 
-
-    //Movement direction when clicked (by Jaber)
-    speed = 1.4;
-
     PlayerMovement();
     EnemyMovement();
-    
+    //Vs AI
+    //AIMovement();
+
     requestAnimationFrame(function () {
         update(myRenderer, myScene, myCamera);
     })
 }
 
+//Movement direction when clicked (by Jaber)
+speed = 2;
+
+//Vs AI
+/*
+function AIMovement() {
+    //enemy.position.y += speed;
+    player.position.y = ball.position.y;   
+}
+*/
+
+//2 players
 function PlayerMovement() {
     if (upPressed && enemy.position.y < topBorder) {
         enemy.position.y += speed;
@@ -296,6 +391,7 @@ function PlayerMovement() {
         enemy.position.y -= speed;
     }
 }
+
 
 function EnemyMovement() {
     if (upPressed2 && player.position.y < topBorder) {
@@ -318,7 +414,4 @@ function RandomBallDirection() {
     direction.normalize();
 }
 
-
-
 var myScene = init();
-
